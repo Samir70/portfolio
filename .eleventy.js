5e97ddae5152a7b67142801fcdd6850f282a3255
Samir70 <sbetmouni@talktalk.net>
2023-04-26 12:09:19 +0100

    add hello world template

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+    // eleventyConfig is the default configuration object that Eleventy provides
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+}
